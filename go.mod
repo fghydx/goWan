04afbecb2c5e33c5de13877cf5ddcdb0873ab1e4
fghydx <5909386@qq.com>
2022-06-20 14:34:41 +0800

    123456

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fghydx/goWan
+
+go 1.18
+
+require github.com/fghydx/gobone v0.0.0-20220620061301-9d79cfc1a4e5 // indirect
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "net"
+
+type test1 struct {
+}
+
+func (t test1) OnConnect(conn net.Conn) bool {
+	println("连接上来了")
+	return true
+}
+func (t test1) OnDisconnect(conn net.Conn) {
+	println("断开连接")
+}
+func (t test1) OnError(conn net.Conn, err error) {
+	println("连接错误")
+}
+func (t test1) OnRecv(conn net.Conn, Adata []byte, len int) bool {
+	println("接收数据")
+	return true
+}
+
+func main() {
+	tcpframe := NewGLNetFrame("127.0.0.1", 9997, test1{})
+	tcpframe.Start()
+}
